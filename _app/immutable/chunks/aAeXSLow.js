5887a70d00360e1f7c92e8a460c4b155e86829cb
vfxTJ-Laptop <vfxturjo@gmail.com>
2025-05-17 03:42:07 +0600

    Updates

---
+++ _app/immutable/chunks/aAeXSLow.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1dq2nna)==null?void 0:s.base)??"/Money-Splitter";var a;const t=((a=globalThis.__sveltekit_1dq2nna)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.DhGYPreD.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CTN6PlD_.js";export{o as load_css,r as start};
