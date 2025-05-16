515492c8e2b8e53f99c7f5b2e71dd58431fe549c
vfxTJ-Laptop <vfxturjo@gmail.com>
2025-05-17 04:31:30 +0600

    Updates

---
+++ _app/immutable/chunks/Be9VlbhD.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_1ollg5u)==null?void 0:s.base)??"/Money-Splitter";var e;const t=((e=globalThis.__sveltekit_1ollg5u)==null?void 0:e.assets)??l;export{t as a,l as b};
--- _app/immutable/entry/start.DxE8yzbE.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Bq_FZ0iH.js";export{o as load_css,r as start};
